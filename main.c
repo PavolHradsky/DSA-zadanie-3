163580388e93962727502c3fa618ac7f386bd8c7
Pavol Hradsky <hradskypalo@gmail.com>
2021-04-12 19:54:13 +0200

    created structures

--- main.c
+++ main.c
@@ -1,6 +1,39 @@
-#include <stdio.h>
+/*********************************************
+ * DSA Zadianie 3 - Binary decision diagram
+ *
+ * FIIT STU 2021
+ * Pavol Hradsky
+*********************************************/
 
-int main() {
-    printf("Hello, World!\n");
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct node {
+    struct node *left;
+    struct node *right;
+    struct node *parent;
+    char *data;
+} NODE;
+
+typedef struct bdd {
+    struct node *head;
+}BDD;
+
+NODE *create_node(char *data){
+    NODE *result = (NODE*)malloc(sizeof(NODE));
+    strcpy(result->data, data);
+    result->left = NULL;
+    result->right = NULL;
+    result->parent = NULL;
+
+    return result;
+}
+
+int main()
+{
+
+    printf("Hello World");
+
     return 0;
 }
